cf3b9ed7274284cc9af38b246fc99ce3737ed561
Rafeeq CE <rafeeq.ebrahim@emirates.com>
2019-07-14 13:49:10 +0530

    Initial Commit

---
+++ GoContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoContacts
+//
+//  Created by Rafeeq Ebrahim on 14/07/19.
+//  Copyright © 2019 Rafeeq Ebrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
